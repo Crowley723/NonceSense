ef4357ade75a58caa35721a9fe076554df09c9d9
Brynn Crowley <littlehill723@gmail.com>
2025-11-13 18:34:07 -0800

    feat: add tanstack router and sidenav

---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { AppSidebar } from "@/components/app-sidebar.tsx";
+import { Separator } from "@/components/ui/separator.tsx";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar.tsx";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+
+      <TanStackRouterDevtools />
+    </>
+  ),
+});
